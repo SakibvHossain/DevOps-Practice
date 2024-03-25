48632d853a9d6e1e00c2531d06ae394a20432745
SakibvHossain <sakibxhossian021@gmail.com>
2024-03-25 23:13:02 +0600

    feat: Add SLF4J logger

--- src/main/java/com/example/sampleproject/SpringBootDevOpsApplication.java
+++ src/main/java/com/example/sampleproject/SpringBootDevOpsApplication.java
@@ -1,12 +1,15 @@
 package com.example.sampleproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDevOpsApplication {
-
+   public static Logger log = LoggerFactory.getLogger(SpringBootDevOpsApplication.class);
 	public static void main(String[] args) {
+		log.info("Application Executed");
 		SpringApplication.run(SpringBootDevOpsApplication.class, args);
 	}
 
